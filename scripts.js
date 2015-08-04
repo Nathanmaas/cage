5648db1acd85797821e05e52b5707448470e0011
Nathan Maas <nathansmaas@gmail.com>
2015-08-03 18:32:41 -0700

    updating js

--- scripts.js
+++ scripts.js
@@ -1,60 +1,140 @@
 $(function(){
-var board = $("#cards")
-var clickSave = false;
-var gif = [   
-"images/nick1.gif",
-"images/nick2.gif",
-"images/nick11.gif",
-"images/nick4.gif",
-"images/nick5.gif",
-"images/nick6.gif",
-"images/nick7.gif",
-"images/nick8.gif",
-"images/nick9.gif",
-"images/nick10.gif",
-"images/nick1.gif",
-"images/nick2.gif",
-"images/nick11.gif",
-"images/nick4.gif",
-"images/nick5.gif",
-"images/nick6.gif",
-"images/nick7.gif",
-"images/nick8.gif",
-"images/nick9.gif",
-"images/nick10.gif",
-];
+	$('#intro').modal('show');
 
-// sort method - Math.random function to shuffle cards
-function shuffle(){
-	gif.sort(function(a, b){
-		return Math.random() - 0.5;
-	});
-}
-shuffle();
 
-// event handler - .on() method to listen for clicks on .gifs
-$(board).on('click', '.gif', function(){
-	var relate = parseInt($(this).attr("id").replace("c", "")) -1;
+	var running = true;
+	var gameBoard = []
+	var clickCount = 0
+	var score = 0
+	var oneScore = 0
+	var twoScore = 0
+	var playerOne= true
 
-	$(this).css("background-image", ("url(./" + gif[relate] + ")"));
-	
+	var clickOne =""
+	var clickTwo =""
+	var classOne =""
+	var classTwo =""
 
-	// pair checker to determine when two gifs have been clicked in a row
-	if (gif[clickSave] === gif[relate] && clickSave !== relate){
-		$(this).hide();
-		$("#c" + (clickSave + 1)).hide();
+	shuffleGame();
+
+	//Gifolas Shuffle Function
+	function shuffleGame(){
+		running = true
+		$('img').addClass("visibility");
+		score = 0
+		clickCount = 0
+		var parent = $(".game-wrapper");
+    	var divs = parent.children();
+    	while (divs.length) {
+        	parent.append(divs.splice(Math.floor(Math.random()
+        	* divs.length), 1)[0]);
+		}
 	}
-		clickSave = relate;
-	
-	})
 
- // event handler - .on() method to reset and shuffle new game (cageon)
- $('#newgame').on('click', function(){
- 	clickSave = false;
- 	shuffle();
- 	$('.gif').show().css("background-image", "url(./images/spade_card.jpeg)");
 
- 	})
+
+	//Gifolas Play Again Function
+	$('.shuffle').click(function (){
+		//console.log('SHUFFLING!')
+		$('img').addClass("visibility");
+		shuffleGame();
+		$('.score').text("");
+	 	$('.score').append('No clicks.');
+	 	$('h3').text('');
+	 	$('h3').append("Player 1's turn!")
+		playerOne = true
+		running = true;
+
+	});
+
+	// function removeClass(){
+	// 	classTwo.addClass("visibility");
+
+	// }
+
+	//Gifolas Matching Function
+	 $('.game-wrapper').find('div').on('click', function(){
+	 	console.log(gameBoard.length)
+		console.log(playerOne)
+	 	if (running === true){
+	 		score = score + 1;
+	 	}
+	 	$('.score').text('');
+	 	$('.score').append(score + ' clicks.');
+	 	//console.log(score)
+	 	if(running === false){
+	 		return;
+	 	}
+	 	if((clickCount % 2 === 0) && ($('img',this).attr('class') === 'visibility')) {
+	 		clickOne = $('img',this).attr('src');
+	 		//console.log(clickOne);
+	 		classOne = $('img',this);
+	 		//clickOne = $('img',this).val();
+	 		clickCount = clickCount + 1;
+	 		//score = score + 1;
+	 		$('img', this).removeClass("visibility");
+	 	}else if((clickCount % 2 != 0) && ($('img',this).attr('class') === 'visibility')){
+	 		clickTwo = $('img',this).attr('src');
+	 		//console.log(clickTwo);
+	 		classTwo = $('img',this);
+	 		//clickTwo = $('img',this).val();
+	 		clickCount = clickCount + 1;
+	 		//score - score + 1
+	 		//setInterval(removeClass, 500);
+	 		$('img', this).removeClass("visibility");
+	 			if(clickOne != clickTwo){
+	 				//classOne.addClass("visibility");
+	 				setTimeout(function(){
+	 					classOne.addClass("visibility");
+       					classTwo.addClass("visibility");
+  						 }, 300);
+	 				//classTwo.addClass("visibility");
+	 				//console.log(setInterval(removeClass, 500));
+	 			} else {
+	 				gameBoard.push(clickOne, clickTwo)
+	 				checkEnd();
+	 				return;
+	 			}
+	 		}
+
+      });
+
+	 function checkEnd(){
+	 	if (gameBoard.length != 16){
+	 		return;
+	 	} else if (playerOne === true) {
+	 		oneScore = score
+	 		alert("It is Player 2's turn!");
+	 		running = false;
+	 		playerOne = false
+	 		$('h3').text('');
+	 		$('h3').append("Player 2's turn!")
+	 		gameBoard = []
+	 		shuffleGame();
+	 	} else {
+	 		twoScore = score
+	 		running = false;
+	 			if (oneScore < twoScore){
+	 				$('#WinnerOne').modal('show');
+	 				$('h3').text('');
+	 				$('h3').append("Player 1 Wins!")
+	 			}
+	 			else if(twoScore < oneScore){
+	 				$('#WinnerTwo').modal('show');
+	 				$('h3').text('');
+	 				$('h3').append("Player 2 Wins!")
+	 			}
+	 			else{
+	 				$('#WinnerTie').modal('show');
+	 				$('h3').text('');
+	 				$('h3').append("It's a tie!")
+	 			}
+
+	 	}
+	 };
+
+
+	//
 });
 
 
